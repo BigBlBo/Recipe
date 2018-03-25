adde7e26078761700e30c45f938edc758f96e2f2
Blaz Bozjak <blaz.bozjak@gmail.com>
2018-03-25 10:02:49 +0200

    initial

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simple a test',
+      'https://www.campbellsoup.co.uk/img/recipes/6-campbells-vegetarian-pizza-recipe.jpg'),
+      new Recipe('A test recipe', 'This is simple a test',
+      'https://www.campbellsoup.co.uk/img/recipes/6-campbells-vegetarian-pizza-recipe.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
